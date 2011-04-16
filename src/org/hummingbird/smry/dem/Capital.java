294fbc9275c8883abd1e511b87cd06ea4ced79f7
Advenis Calculo <advenis@live.com>
2011-04-16 02:33:40 -0400

    redem. manifest commit

---
+++ src/org/hummingbird/smry/dem/Capital.java
@@ -0,0 +1,5 @@
+package org.hummingbird.smry.dem;
+
+public class Capital {
+
+}
---
+++ src/org/hummingbird/smry/rep/Viz.java
@@ -0,0 +1,18 @@
+package org.hummingbird.smry.rep;
+
+/**
+ * <p>Serves as rep. interfacing structure
+ * - calls for videlicit.</p>
+ * 
+ * @author Advenis Calculo
+ */
+public interface Viz {
+
+	/**
+	 * Retrieves formatted index.
+	 * @return An index pertaining to enlistment
+	 * inside it's {@link org.hummingbird.smry.dem.Capital}
+	 */
+	public int getFormmatedIndex();
+	
+}
