fda7baed7aefb74b6ae6a7c1c8be3322f6fda554
Advenis Calculo <advenis@live.com>
2011-04-16 01:54:12 -0400

    entry point commit

---
+++ src/org/hummingbird/Main.java
@@ -0,0 +1,13 @@
+package org.hummingbird;
+
+/**
+ * Entry point: #<tt>849-&6</tt>
+ * @author Advenis Calculo
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// FILL: <?>Ajsu&@^? -- ¤=5%</?>
+	}
+	
+}
